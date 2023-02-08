748b0676ff1a25e38800584e104f2510e30bac13
Kevin Kretz <kkretz17+github@gmail.com>
2023-02-08 16:54:11 +0000

    navigation fully complete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 // Open and close nav bar menu on mobile
-function toggleMenu() {
-    var element = document.getElementById("mobile-nav-menu");
-    element.classList.toggle("hidden");
-}
+const navList = document.querySelector(".nav-list");
+const mobileNavButton = document.querySelector("nav>button");
+const menuIcon = document.querySelector(".menu_icon");
+const closeIcon = document.querySelector(".close_icon");
+const navLogo = document.querySelector(".logo")
+
+mobileNavButton.addEventListener("click", () => {
+    const visible = navList.getAttribute("data-visible");
+
+    if (visible == "initial" || visible=="false") {
+        navList.setAttribute("data-visible", "true");
+        mobileNavButton.setAttribute("aria-expanded", "true");
+        navLogo.classList.remove("fadeIn");
+        navLogo.classList.add("fadeOut");
+        menuIcon.classList.add("hidden");
+        closeIcon.classList.remove("hidden");
+    }
+    else if (visible == "true"){
+        navList.setAttribute("data-visible", "false");
+        mobileNavButton.setAttribute("aria-expanded", "false");
+        navLogo.classList.remove("fadeOut");
+        navLogo.classList.add("fadeIn");
+        closeIcon.classList.add("hidden");
+        menuIcon.classList.remove("hidden");
+    }
+    console.log(visible);
+})
 
 // Get year for copyright in footer.
 document.getElementById("year").innerHTML = new Date().getFullYear();
