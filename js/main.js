7cc04003d7845a7a2c6c662adfa40462b712ed21
Kevin Kretz <kkretz17+github@gmail.com>
2023-02-08 00:59:39 +0000

    working mobile nav menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+// Open and close nav bar menu on mobile
+function toggleMenu() {
+    var element = document.getElementById("mobile-nav-menu");
+    element.classList.toggle("hidden");
+}
+
+// Get year for copyright in footer.
 document.getElementById("year").innerHTML = new Date().getFullYear();
