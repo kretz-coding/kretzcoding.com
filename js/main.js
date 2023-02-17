75a6e9d16a88400d021d251ae754e34e5e4f5e54
Kevin Kretz <kkretz17+github@gmail.com>
2023-02-17 02:17:29 +0000

    nav complete - mobile first design

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 // Open and close nav bar menu on mobile
-const navList = document.querySelector(".nav-list");
-const mobileNavButton = document.querySelector("nav>button");
+const navList = document.querySelector("nav .list");
+const navToggleButton = document.querySelector("nav .toggle-button");
 const menuIcon = document.querySelector(".menu_icon");
 const closeIcon = document.querySelector(".close_icon");
-const navLogo = document.querySelector(".logo")
+const navLogo = document.querySelector("nav .logo")
 
-mobileNavButton.addEventListener("click", () => {
-    const visible = navList.getAttribute("data-visible");
+navToggleButton.addEventListener("click", () => {
+    const visibility = navList.getAttribute("data-visible");
 
-    if (visible == "initial" || visible=="false") {
+    if (visibility == "initial" || visibility=="false") {
         navList.setAttribute("data-visible", "true");
-        mobileNavButton.setAttribute("aria-expanded", "true");
+        navToggleButton.setAttribute("aria-expanded", "true");
         navLogo.classList.remove("fadeIn");
         navLogo.classList.add("fadeOut");
         menuIcon.classList.add("hidden");
         closeIcon.classList.remove("hidden");
     }
-    else if (visible == "true"){
+    else if (visibility == "true"){
         navList.setAttribute("data-visible", "false");
-        mobileNavButton.setAttribute("aria-expanded", "false");
+        navToggleButton.setAttribute("aria-expanded", "false");
         navLogo.classList.remove("fadeOut");
         navLogo.classList.add("fadeIn");
         closeIcon.classList.add("hidden");
         menuIcon.classList.remove("hidden");
     }
-    console.log(visible);
 })
 
 // Get year for copyright in footer.
