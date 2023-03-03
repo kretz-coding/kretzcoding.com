2b87a5a3a22b1316436f3db05c29912d4bfe22c9
Kevin Kretz <kkretz17+github@gmail.com>
2023-03-03 06:22:51 +0000

    added function to get selected bubble inner text

--- js/home.js
+++ js/home.js
@@ -1,10 +1,17 @@
-var bubbles = document.getElementsByClassName("bubble");
+const bubbles = document.getElementsByClassName("bubble");
 
-var toggleSelected = function() {
+let toggleSelected = function() {
     this.classList.toggle("selected");
-    console.log("toggled");
 };
 
 for (var i = 0; i < bubbles.length; i++) {
     bubbles[i].addEventListener('click', toggleSelected, false);
+}
+
+function test() {
+    let selectedIssues = document.getElementsByClassName("bubble selected");
+    
+    for (let i = 0; i < selectedIssues.length; i++) {
+        console.log (selectedIssues[i].innerText);
+    }
 }
