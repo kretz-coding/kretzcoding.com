514a779c9a5aaa3c868187fdc80f8ed98ec43efe
kevin-kretz <kkretz17@gmail.com>
2023-06-14 09:03:17 -0800

    added src files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar.js";
+
+export default function App() {
+  return (
+    <div className="dark_theme">
+      <Navbar />
+    </div>
+  );
+}
